c53ca04fcd4efe4f005e4cc94e42872d353f50dd
Shahzer Baig <shahzer.baig.sb@gmail.com>
2022-11-24 18:47:17 +0530

    singleCoinPage Basecode Added

---
+++ src/Components/CoinInfo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const CoinInfo = (props) => {
+    
+    return (
+        <div>
+            CoinInfo
+        </div>
+    )
+}
--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,9 +1,130 @@
-import React from 'react'
-
+import React,{useState, useContext, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { Crypto } from '../contextApi/CryptoContext';
+import {SingleCoin} from '../Config/api'
+import axios from 'axios';
+import { css } from '@emotion/css';
+import { CoinInfo } from '../Components/CoinInfo';
+import { LinearProgress,Typography } from '@mui/material';
+import * as parse from  'html-react-parser'
 export const CoinPage = () => {
+    const { id } = useParams();
+
+    //Internal State
+    const [coin, setCoin] = useState();
+    const {currency, symbol} = useContext(Crypto);
+
+    // get Api Call
+    const fetchCoin = async ()=>{
+        const {data} = await axios.get(SingleCoin(id))
+        setCoin(data)
+    }
+    useEffect(() => {
+        fetchCoin()
+    }, [])
+   // Css Styles
+//    const container =(theme)=> css({
+//        color:"purple",
+//        display:"flex",
+//        [theme.breakpoint.down("md")]:{
+//            flexDirection:"column",
+//            alignItems:"center"
+//        }
+//    });
+    const container = css`
+        display:flex;
+        flex-direction: row;
+        
+    `;
+   console.log(container);
+   const sideBar = css`
+    display:flex;
+    flex-direction:column;
+    alignItem:center;
+    marging-top:25;
+    border-right:2px solid grey;
+
+   `;
+   const heading = css`
+        font-weight:bold;
+        font-size:2rem;
+        margin-bottom:20;
+        font-family:Montserrat;
+        text-align:center
+   `;
+   const description = css`
+        width:100%;
+        font-family:Montserrat;
+        text-align:justify;
+        padding:25;
+   `;
+   const marketData = css`
+        align-self:start;
+        padding:25;
+        paddingTop:10;
+        width:100%;
+   `;
+   if(!coin){
+       return(
+           <LinearProgress style={{backgroundColor:"gold"}}/>
+       )
+   }
     return (
-        <div>
-            <h1>Coin Page</h1>
+        <div className={container}>
+            <div className={sideBar}>
+              <img 
+                src={coin?.image.large} 
+                alt={coin?.name}
+                height='200'
+                width='200'
+                style={{
+                    margin:"0 auto",
+
+                    marginBottom:20,
+
+                }}/>
+                <Typography varient="h3" className={heading}>
+                    {coin?.name}
+                </Typography>
+                <Typography varient="subtitle1" className={description}>
+                {coin?.description.en.split(".")[0]}
+                </Typography>
+                <div className={marketData}>
+                    <span style={{display:"flex", justifyContent:"center"}}>
+                        <Typography variant="h5" className={heading}>
+                            Rank:
+                        </Typography>
+                        &nbsp;&nbsp;
+                        <Typography varient="h5" style={{fontFamily:"Montserrat",fontSize:"1.8rem"}}>
+                            {coin?.market_cap_rank}
+                        </Typography>
+                    </span>
+                    <span style={{display:"flex", margin:"0 auto", justifyContent:"space-evenly"}}>
+                        <Typography variant="h5" className={heading}>
+                            Current Price:
+                        </Typography>
+                        &nbsp;&nbsp;
+                        <Typography varient="h5" style={{fontFamily:"Montserrat",fontSize:"1.8rem"}}>
+                            {symbol}{" "}
+                            {coin?.market_data.current_price[currency.toLowerCase()]}
+                        </Typography>
+                    </span>
+
+                    <span style={{display:"flex", justifyContent:"space-evenly"}}>
+                        <Typography variant="h5" className={heading} style={{fontFamily:"Montserrat",fontSize:"1.8rem"}}>
+                            Market Cap:
+                        </Typography>
+                        &nbsp;&nbsp;
+                        <Typography varient="h5" style={{fontFamily:"Montserrat",fontSize:"1.8rem"}}>
+                            {symbol}&nbsp;{coin?.market_data.market_cap[currency.toLowerCase()]
+                                        .toString()
+                                        .slice(0,-6)}M
+                        </Typography>
+                    </span>
+                </div>
+            </div>
+            {/* chart */}
+            <CoinInfo coin={coin}/>
         </div>
     )
 }
